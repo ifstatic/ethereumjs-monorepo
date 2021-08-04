fadfb3af487c41b29cb49eda0b3892ca6130eade
Ryan Ghods <ryan@ryanio.com>
2021-08-03 19:53:10 -0700

    add resolutions to root package.json

--- scripts/e2e-inject-resolutions.js
+++ scripts/e2e-inject-resolutions.js
@@ -1,18 +1,24 @@
 /*
   This script injects the `resolutions.json` file created by
-  the e2e-resolutions.js script into hardhat-core's `package.json`.
+  the e2e-resolutions.js script into hardhat's root `package.json`.
  */
 
 const fs = require('fs')
 
 const resolutions = require(`${process.cwd()}/resolutions.json`)
 
-const corePackageJsonLocation = `${process.cwd()}/hardhat/packages/hardhat-core/package.json`
-const corePackageJson = require(corePackageJsonLocation)
+// for yarn workspaces, resolutions need to be
+// placed in the root package.json
+// (https://github.com/yarnpkg/yarn/issues/5039#issuecomment-880166818)
+const rootPackageJsonLocation = `${process.cwd()}/hardhat/package.json`
+const rootPackageJson = require(rootPackageJsonLocation)
 
-const newCorePackageJson = { ...corePackageJson, resolutions }
+const newRootPackageJson = { ...rootPackageJson, resolutions }
 
-newCorePackageJson.resolutions['@types/bn.js'] = '5.1.0' // adds specific module that hardhat can't find by default
-newCorePackageJson.resolutions['eth-sig-util'] = '3.0.1' // resolves provider eth_signTypedData_v4 error
+newRootPackageJson.resolutions['@types/bn.js'] = '5.1.0' // adds specific module that hardhat can't find by default
+newRootPackageJson.resolutions['eth-sig-util'] = '3.0.1' // resolves provider eth_signTypedData_v4 error
 
-fs.writeFileSync(corePackageJsonLocation, JSON.stringify(newCorePackageJson, null, 2))
+fs.writeFileSync(
+  rootPackageJsonLocation,
+  JSON.stringify(newRootPackageJson, null, 2)
+)
